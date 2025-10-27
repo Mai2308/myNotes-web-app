117d4689a4fb1915af0fb60d9ab683199e95d28d
Mai Taha <M.Amr2308@nu.edu.eg>
2025-10-27 17:23:18 +0300

    edited the sql server to the correct password

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const poolConnect = sql.connect({
   user: process.env.DB_USER || "sa",           // <- change 'root' to 'sa'
-  password: process.env.DB_PASSWORD || "YourStrong!Passw0rd", // <- must match Docker
+  password: process.env.DB_PASSWORD || "YourStrong!Pass123", // <- must match Docker
   server: process.env.DB_HOST || "localhost",
   database: process.env.DB_NAME || "NotesApp",
   options: {
